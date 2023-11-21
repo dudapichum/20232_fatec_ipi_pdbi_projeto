5120081dfa506d7507250ad15b1aa83a6faf7f57
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-11-21 10:54:07 -0300

    ap13(projeto) código: 1.4.3

--- script.sql
+++ script.sql
@@ -1,15 +1,38 @@
--- 1.4.2 Exibir o percentual de estudantes de cada sexo
-CREATE OR REPLACE PROCEDURE sp_percentage_by_gender
-LANGUAGE plpgsql
+-- 1.4.3 Receber um sexo como parâmetro em modo IN e utilizar oito parâmetros em modo OUT para dizer qual o percentual de cada nota obtida por estudantes daquele sexo.
+CREATE OR REPLACE PROCEDURE sp_percentage_notes_by_gender(
+    IN p_gender VARCHAR(10),
+    OUT p_note_0_1 NUMERIC,
+    OUT p_note_1_2 NUMERIC,
+    --... outros parâmetros OUT para notas
+    OUT p_note_9_10 NUMERIC
+) LANGUAGE plpgsql
 AS $$
 BEGIN
-    RAISE NOTICE 'Percentual de estudantes por sexo:';
-    PERFORM
-        RAISE NOTICE 'Masculino: %', (COUNT(*) FILTER (WHERE GENDER = 'M') * 100.0 / COUNT(*));
-    PERFORM
-        RAISE NOTICE 'Feminino: %', (COUNT(*) FILTER (WHERE GENDER = 'F') * 100.0 / COUNT(*));
+    SELECT
+        (COUNT(*) FILTER (WHERE GENDER = p_gender AND GRADE BETWEEN 0 AND 1) * 100.0 / COUNT(*)),
+        --... calcula outros percentuais de notas
+        (COUNT(*) FILTER (WHERE GENDER = p_gender AND GRADE BETWEEN 9 AND 10) * 100.0 / COUNT(*))
+    INTO
+        p_note_0_1,
+        --... atribuição para outros parâmetros OUT
+        p_note_9_10
+    FROM student_performance;
 END;
 $$;
+
+
+-- 1.4.2 Exibir o percentual de estudantes de cada sexo
+-- CREATE OR REPLACE PROCEDURE sp_percentage_by_gender
+-- LANGUAGE plpgsql
+-- AS $$
+-- BEGIN
+--     RAISE NOTICE 'Percentual de estudantes por sexo:';
+--     PERFORM
+--         RAISE NOTICE 'Masculino: %', (COUNT(*) FILTER (WHERE GENDER = 'M') * 100.0 / COUNT(*));
+--     PERFORM
+--         RAISE NOTICE 'Feminino: %', (COUNT(*) FILTER (WHERE GENDER = 'F') * 100.0 / COUNT(*));
+-- END;
+-- $$;
 
 
 -- 1.4 Escrever os stored procedures
