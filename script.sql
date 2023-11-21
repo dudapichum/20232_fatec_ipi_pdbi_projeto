1c4a25ab945c35e41d58bec1b1b1a9c97832a1e0
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-11-21 10:53:26 -0300

    ap13(projeto) código: 1.4.2

--- script.sql
+++ script.sql
@@ -1,15 +1,29 @@
--- 1.4 Escrever os stored procedures
--- 1.4.1 Exibir o número de estudantes maiores de idade
-CREATE OR REPLACE PROCEDURE sp_students_over_age
+-- 1.4.2 Exibir o percentual de estudantes de cada sexo
+CREATE OR REPLACE PROCEDURE sp_percentage_by_gender
 LANGUAGE plpgsql
 AS $$
-DECLARE
-    v_count INT;
 BEGIN
-    SELECT COUNT(*) INTO v_count FROM student_performance WHERE AGE >= 18;
-    RAISE NOTICE 'Número de estudantes maiores de idade: %', v_count;
+    RAISE NOTICE 'Percentual de estudantes por sexo:';
+    PERFORM
+        RAISE NOTICE 'Masculino: %', (COUNT(*) FILTER (WHERE GENDER = 'M') * 100.0 / COUNT(*));
+    PERFORM
+        RAISE NOTICE 'Feminino: %', (COUNT(*) FILTER (WHERE GENDER = 'F') * 100.0 / COUNT(*));
 END;
 $$;
+
+
+-- 1.4 Escrever os stored procedures
+-- 1.4.1 Exibir o número de estudantes maiores de idade
+-- CREATE OR REPLACE PROCEDURE sp_students_over_age
+-- LANGUAGE plpgsql
+-- AS $$
+-- DECLARE
+--     v_count INT;
+-- BEGIN
+--     SELECT COUNT(*) INTO v_count FROM student_performance WHERE AGE >= 18;
+--     RAISE NOTICE 'Número de estudantes maiores de idade: %', v_count;
+-- END;
+-- $$;
 
 
 -- 1.3 Copiando os dados do arquivo .csv para a tabela
