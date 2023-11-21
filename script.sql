23e435328c4043f05d78d2c95e5c6287a498a7d4
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-11-21 10:52:42 -0300

    ap13(projeto) código: 1.4

--- script.sql
+++ script.sql
@@ -1,13 +1,27 @@
+-- 1.4 Escrever os stored procedures
+-- 1.4.1 Exibir o número de estudantes maiores de idade
+CREATE OR REPLACE PROCEDURE sp_students_over_age
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_count INT;
+BEGIN
+    SELECT COUNT(*) INTO v_count FROM student_performance WHERE AGE >= 18;
+    RAISE NOTICE 'Número de estudantes maiores de idade: %', v_count;
+END;
+$$;
+
+
 -- 1.3 Copiando os dados do arquivo .csv para a tabela
-COPY Students (AGE, GENDER, SALARY, PREP_EXAM, NOTES, GRADE) FROM 'C:\Users\aluno\Documents\MariaEduarda\pdbi\projeto' DELIMITER ',' CSV HEADER;
+-- COPY Students (AGE, GENDER, SALARY, PREP_EXAM, NOTES, GRADE) FROM 'C:\Users\aluno\Documents\MariaEduarda\pdbi\projeto' DELIMITER ',' CSV HEADER;
 
 -- PROJETO
 -- 1.2 Criar uma tabela apropriada
-CREATE TABLE student_performance (
-    AGE INT,
-    GENDER VARCHAR(10),
-    SALARY NUMERIC,
-    PREP_EXAM VARCHAR(50),
-    NOTES VARCHAR(100),
-    GRADE NUMERIC
-);
+-- CREATE TABLE student_performance (
+--     AGE INT,
+--     GENDER VARCHAR(10),
+--     SALARY NUMERIC,
+--     PREP_EXAM VARCHAR(50),
+--     NOTES VARCHAR(100),
+--     GRADE NUMERIC
+-- );
