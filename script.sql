a0dd58ac3e5da9ee63bd3285ee4a6be746c295a2
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-11-21 10:50:13 -0300

    ap13(projeto) código: 1.3

--- script.sql
+++ script.sql
@@ -1,3 +1,7 @@
+-- 1.3 Copiando os dados do arquivo .csv para a tabela
+COPY Students (AGE, GENDER, SALARY, PREP_EXAM, NOTES, GRADE) FROM 'C:\Users\aluno\Documents\MariaEduarda\pdbi\projeto' DELIMITER ',' CSV HEADER;
+
+-- PROJETO
 -- 1.2 Criar uma tabela apropriada
 CREATE TABLE student_performance (
     AGE INT,
