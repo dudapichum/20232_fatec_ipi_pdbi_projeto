26e28f32f1a6e396601efc864fdc369bdc09cf5e
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-11-21 10:48:41 -0300

    ap13(projeto) código: 1.2

---
+++ script.sql
@@ -0,0 +1,9 @@
+-- 1.2 Criar uma tabela apropriada
+CREATE TABLE student_performance (
+    AGE INT,
+    GENDER VARCHAR(10),
+    SALARY NUMERIC,
+    PREP_EXAM VARCHAR(50),
+    NOTES VARCHAR(100),
+    GRADE NUMERIC
+);
